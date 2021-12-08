efc6afdcde458bb46e7c34cd32903796c8231a3d
Ubuntu <Rianna@Development-server.bmsm3o2w23qedo50yba1ml1ilh.bx.internal.cloudapp.net>
2021-12-08 14:59:25 +0000

    completed create task

--- application/routes.py
+++ application/routes.py
@@ -1,16 +1,21 @@
 from application import app, db
 from application.models import Tasks
-from flask import render_template
+from application.forms import TaskForm
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 def home():
-    all_tasks = Tasks.query.all()
+    all_tasks = Tasks.query.all() # retrieve list of tasks from db
     return render_template("index.html", title="Home", all_tasks=all_tasks)
 
-@app.route('/create_task') 
+@app.route('/create_task', methods=["GET", "POST"])
 def create_task():
-    new_task = Tasks(description="New Task")
-    db.session.add(new_task)
-    db.session.commit()
+    form = TaskForm()
 
-    return  f"Task added with ID {new_task.id}"
+    if request.method == "POST":
+        new_task = Tasks(description=form.description.data)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for("home")) #home is the name of the functiion
+
+    return render_template("create_task.html", title="Add a Task", form=form)
